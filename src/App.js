37a7966925b0a39ea5a160173204599141bdd7f2
Renaud Bordet <rbordet84@gmail.com>
2020-12-09 00:14:38 +0100

    6. Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AboutUs from './pages/AboutUs'
 // GLOBAL STYLE
 import GlobalStyle from './components/GlobalStyle';
+import Nav from './/components/Nav';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
